28d6bbeecae0ee2d2a806e919670a1151489c4de
Pietro <marrazzo.pietroalfonso@gmail.com>
2020-10-29 14:37:26 +0100

    show displayer e timer

--- js/main.js
+++ js/main.js
@@ -3,10 +3,15 @@ $(document).ready(function(){
     // creo due array, uno per il pc, uno per il giocatore
     var numMax = 5;
     var difficult = 50;
+    var time = 10;
     var numPC = [];
     var numPlayer = [];
     var start = $('.flex-wrap .btn');
     var numRng = 0;
+    var num = 0;
+    var numbers = $('.flex-wrap .numbers');
+
+
     // console.log(numPC, numPlayer);
     
     // genero 5 numeri casuali e li inserisco nell'array
@@ -18,8 +23,21 @@ $(document).ready(function(){
         } 
         console.log(numPC);
 
-        // espongo i numeri con un alert
-        alert('Guarda e ricorda! \n' + numPC)
+
+
+        alert('Guarda e ricorda! \n' + numPC);
+        
+
+        // espongo i numeri con un alert per 30 secondi - 7 di per prova
+        setTimeout(function(){
+
+            
+            num = prompt('Inserisci ');
+
+        }, 7000);
+        
+
+
 
     });
 
